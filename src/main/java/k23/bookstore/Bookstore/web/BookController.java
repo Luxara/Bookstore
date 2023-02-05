bc045d68f9608833b08968d479fef6568e8eab10
Lux <luxara-sama@hotmail.fi>
2023-02-05 16:23:01 +0200

    Added a booklist view

--- src/main/java/k23/bookstore/Bookstore/web/BookController.java
+++ src/main/java/k23/bookstore/Bookstore/web/BookController.java
@@ -1,14 +1,27 @@
 package k23.bookstore.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import k23.bookstore.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	@RequestMapping(value="/index", method=RequestMethod.GET)
 		public String mainPage(Model model){
 		return "index";
+	}
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value="/booklist", method=RequestMethod.GET)
+		public String showBooks(Model model){
+			model.addAttribute("books", repository.findAll());
+		
+			return "booklist";
 	}
 }
