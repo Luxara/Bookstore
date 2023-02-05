58899cb3f4338cf39a1465b8867fdea4d1d38332
Lux <luxara-sama@hotmail.fi>
2023-02-05 17:28:46 +0200

    Added delete and add functionality

--- src/main/java/k23/bookstore/Bookstore/web/BookController.java
+++ src/main/java/k23/bookstore/Bookstore/web/BookController.java
@@ -3,9 +3,11 @@ package k23.bookstore.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import k23.bookstore.Bookstore.domain.Book;
 import k23.bookstore.Bookstore.domain.BookRepository;
 
 @Controller
@@ -17,11 +19,29 @@ public class BookController {
 	
 	@Autowired
 	private BookRepository repository;
-	
+
 	@RequestMapping(value="/booklist", method=RequestMethod.GET)
 		public String showBooks(Model model){
 			model.addAttribute("books", repository.findAll());
 		
 			return "booklist";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+		public String deleteBook(@PathVariable ("id") Long bookId, Model model){
+			repository.deleteById(bookId);
+			return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value="/add")
+		public String addBook(Model model){
+			model.addAttribute("book", new Book());
+			return "addBook";
+	}
+	
+	@RequestMapping(value="save", method=RequestMethod.POST)
+		public String saveBook(Book book){
+			repository.save(book);
+			return "redirect:booklist";
 	}
 }
